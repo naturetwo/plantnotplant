121ff7c27a3dfaf2b63aabe9c93758771a41069f
MarcusJones <jones.0bj3@gmail.com>
2018-11-24 22:30:44 +0100

    checkpoint I2C / motor control works

--- arduino/test_serial_motors/test_serial_motors.ino
+++ arduino/test_serial_motors/test_serial_motors.ino
@@ -19,61 +19,81 @@ char str[50];
 void setup()  
 {
   Serial.begin(9600);  
-  Serial.println("--- Start Serial Monitor SEND_RCVE ---");
-    Serial.println(" Type in Box above, . ");
-  Serial.println("(Decimal)(Hex)(Character)(Binary)");  
-  Serial.println(); 
+
+  // Stepper pins
+  pinMode(PIN_STEP,OUTPUT);
+  pinMode(PIN_DIRECTION,OUTPUT);
+  
+  // Servo pins
+  servo1.attach(PIN_SERVO);
+  servo1.write(180);  
 }
-//--(end setup )---
 
 void loop()   /****** LOOP: RUNS CONSTANTLY ******/
 {
+  // Process the byte
   if (Serial.available() > 0)
   {
     ByteReceived = Serial.read();
-//    Serial.printf()
-    Serial.print(ByteReceived);   
-    Serial.print("        ");      
-    Serial.print(ByteReceived, HEX);
-    Serial.print("       ");     
-    Serial.print(char(ByteReceived));
-    Serial.print("       ");     
-    Serial.println(ByteReceived, BIN);
-    
-    if(ByteReceived == '1') // Single Quote! This is a character.
-    {
-//      digitalWrite(led,HIGH);
-      Serial.print(" LED ON ");
-    }
-    
-    if(ByteReceived == '0')
-    {
-//      digitalWrite(led,LOW);
-      Serial.print(" LED OFF");
-    }
-    
     switch (ByteReceived) {
       case 'f':
         sprintf(str, "%c : Stepper forward\n", char(ByteReceived));
-        Serial.print(str); 
+        Serial.print(str);
+        stepper_rotate(1);
+        break;
+
       case 'b':
         sprintf(str, "%c : Stepper backward\n", char(ByteReceived));
         Serial.print(str);
+        stepper_rotate(-1);        
+        break;
+
+      case '1':
+        sprintf(str, "%c : Servo position 1\n", char(ByteReceived));
+        Serial.print(str);
+        servo1.write(2); // Just above 0 to prevent motor chatter
+        break;
+
+      case '2':
+        sprintf(str, "%c : Servo position 2\n", char(ByteReceived));
+        Serial.print(str);
+        servo1.write(90);        
+        break;
+
+      case '3':
+        sprintf(str, "%c : Servo position 3\n", char(ByteReceived));
+        Serial.print(str);
+        servo1.write(180);        
+        break;
+
       default:
         sprintf(str, "%c : Unrecognized byte!\n", char(ByteReceived));
         Serial.print(str);    
+        break;
       }
-    
-    
-    
-    Serial.println();    // End the line
 
   // END Serial Available
   }
 }
 
-//--(end main loop )---
+void stepper_rotate(float rotations){
+  // Get the direction
+  if (rotations<0){
+    digitalWrite(PIN_DIRECTION,LOW);
+  }
+  else {
+    digitalWrite(PIN_DIRECTION,HIGH);
+  }
+  
+  // Get the required steps
+  int steps = abs(rotations) * STEPS_ROTATE;
+  
+  // Rotate
+  for(int x = 0; x < steps; x++){
+      digitalWrite(PIN_STEP,HIGH);
+      delayMicroseconds(500);
+      digitalWrite(PIN_STEP,LOW);
+      delayMicroseconds(500);
+  }  
+}
 
-/*-----( Declare User-written Functions )-----*/
-
-/*********( THE END )***********/
