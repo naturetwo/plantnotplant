773a590338108d4e569a8b86bb7db8f8970ea3e3
Marcus <jones.0bj3@gmail.com>
2018-11-25 06:21:16 +0000

    update python i2c

--- src/test_i2c.py
+++ src/test_i2c.py
@@ -2,6 +2,13 @@ from smbus import SMBus
 
 addr = 0x8 # bus address
 bus = SMBus(1) # indicates /dev/ic2-1
-bus.write_byte(addr, 0x1) # switch it on
-input("Press return to exit")
-bus.write_byte(addr, 0x0) # switch it on
+# bus.write_byte(addr, 0x1) # switch it on
+# input("Press return to exit")
+# bus.write_byte(addr, 0x0) # switch it on
+while True:
+	val = input("Character byte:")
+	val = str(val)
+	print(val)
+	# print(hex(val))
+	print(hex(ord(val)))
+	bus.write_byte(addr, ord(val)) # switch it on
