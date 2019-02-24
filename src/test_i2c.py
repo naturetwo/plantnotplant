f5228d23cb372d506821481bc17c678c00b18009
MarcusJones <jones.0bj3@gmail.com>
2019-02-24 08:55:44 +0100

    update requirements

--- src/test_i2c.py
+++ src/test_i2c.py
@@ -1,18 +1,24 @@
 #
-# f, b
-# 1, 2, 3
-# w, W
 from smbus import SMBus
 
-addr = 0x8 # bus address
-bus = SMBus(1) # indicates /dev/ic2-1
+ADDR_TRACK_CNTRL = 0x8  # I2C bus address
+ADDR_STEP_CTRL = 0x9  # I2C bus address
+
+I2C_MAP = {8: ADDR_TRACK_CNTRL, 9: ADDR_STEP_CTRL}
+
+bus = SMBus(1)  # indicates /dev/ic2-1
 # bus.write_byte(addr, 0x1) # switch it on
 # input("Press return to exit")
 # bus.write_byte(addr, 0x0) # switch it on
 while True:
-	val = input("Character byte:")
-	val = str(val)
-	print(val)
-	# print(hex(val))
-	print(hex(ord(val)))
-	bus.write_byte(addr, ord(val)) # switch it on
+    this_addr = input("Address (8 or 9)")
+    if this_addr not in [8, 9]:
+        continue
+    print("Address selected:", val)
+    this_pin = input("Pin? (1-13)")
+    if this_pin not in list(range(1,14)):
+        continue
+
+    print(hex(val))
+    print(hex(ord(val)))
+    bus.write_byte(addr, ord(val))  # switch it on
